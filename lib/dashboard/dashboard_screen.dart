ea8be4ce41ab78de27766dabe3f0f91f7799628e
Rebar Ahmad <rebar.ahmad@gmail.com>
2021-09-06 00:14:28 +0200

    Initial Commit

---
+++ lib/dashboard/dashboard_screen.dart
@@ -0,0 +1,27 @@
+import 'package:app/audio_screen/audio_screen.dart';
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          child: const Text('Start Audio Session'),
+          onPressed: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const AudioScreen(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
